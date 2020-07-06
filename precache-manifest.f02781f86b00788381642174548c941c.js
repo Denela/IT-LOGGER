daee39e193336c77ac7b7890c9eb3b9997690992
Derek Gwekwerere <makoregwekwerere@gmail.com>
2020-07-06 15:48:08 +0200

    Updates

---
+++ precache-manifest.f02781f86b00788381642174548c941c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f754863a4498ad877ebb5e33922a7d",
+    "url": "/IT-LOGGER/index.html"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/IT-LOGGER/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "9daf4ee7bf85ca40c6e6",
+    "url": "/IT-LOGGER/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/IT-LOGGER/static/js/2.668ff7c0.chunk.js"
+  },
+  {
+    "revision": "494382bc2e1fa58818bf1f9041d8a9bc",
+    "url": "/IT-LOGGER/static/js/2.668ff7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9daf4ee7bf85ca40c6e6",
+    "url": "/IT-LOGGER/static/js/main.c0d297e1.chunk.js"
+  },
+  {
+    "revision": "e9a25683fb9f8d82ca42",
+    "url": "/IT-LOGGER/static/js/runtime-main.6a6cd286.js"
+  }
+]);
